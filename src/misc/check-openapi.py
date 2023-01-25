2d26425cd7e3af9e34f919f9b6f473466b5fc0c3
Erik Pearson <eapearson@lbl.gov>
2023-01-24 19:14:45 -0800

    black format [CE-162]

--- src/misc/check-openapi.py
+++ src/misc/check-openapi.py
@@ -12,61 +12,68 @@ from orcidlink.main import app
 # code (0).
 #
 
+
 def list_diff(list1: list, list2: list, context=[]):
     if len(list1) != len(list2):
-        raise ValueError(f'list1 and list2 are different lengths: {context}')
+        raise ValueError(f"list1 and list2 are different lengths: {context}")
     sorted1 = sorted(list1)
     sorted2 = sorted(list2)
     for index, el1 in enumerate(sorted1):
         el2 = sorted2[index]
         if isinstance(el1, dict):
-            dict_diff(el1, el2, [*context, {'list': index}])
+            dict_diff(el1, el2, [*context, {"list": index}])
         elif isinstance(el1, list):
-            list_diff(el1, el2, [*context, {'list': index}])
+            list_diff(el1, el2, [*context, {"list": index}])
         elif el1 != el2:
-            raise ValueError(f'element at position {index} is different between the two lists: {context}')
+            raise ValueError(
+                f"element at position {index} is different between the two lists: {context}"
+            )
 
 
 def dict_diff(dict1: dict, dict2: dict, context=[]):
     keys1 = list(dict1.keys())
     keys2 = list(dict2.keys())
-    list_diff(keys1, keys2, [*context, {'dict': 'keys'}])
+    list_diff(keys1, keys2, [*context, {"dict": "keys"}])
     for key in keys1:
         el1 = dict1[key]
         el2 = dict2[key]
         if isinstance(el1, dict):
-            dict_diff(el1, el2, [*context, {'dict': key}])
+            dict_diff(el1, el2, [*context, {"dict": key}])
         elif isinstance(el1, list):
-            list_diff(el1, el2, [*context, {'dict': key}])
+            list_diff(el1, el2, [*context, {"dict": key}])
         elif el1 != el2:
-            raise ValueError(f'element for key {key} is different between the two lists: {context}')
+            raise ValueError(
+                f"element for key {key} is different between the two lists: {context}"
+            )
 
 
 def main():
     with open("/kb/module/docs/api/openapi.json", "r") as fin:
         current_openapi = json.load(fin)
 
-    new_openapi = json.loads(json.dumps(
-        get_openapi(
-            title=app.title,
-            version=app.version,
-            openapi_version=app.openapi_version,
-            description=app.description,
-            routes=app.routes,
-            tags=app.openapi_tags,
-            terms_of_service=app.terms_of_service,
-            contact=app.contact,
-            license_info=app.license_info
+    new_openapi = json.loads(
+        json.dumps(
+            get_openapi(
+                title=app.title,
+                version=app.version,
+                openapi_version=app.openapi_version,
+                description=app.description,
+                routes=app.routes,
+                tags=app.openapi_tags,
+                terms_of_service=app.terms_of_service,
+                contact=app.contact,
+                license_info=app.license_info,
+            )
         )
-    ))
+    )
 
     try:
         dict_diff(current_openapi, new_openapi)
-        print('openapi.json has no changes')
+        print("openapi.json has no changes")
         sys.exit(0)
     except ValueError as ve:
-        print('openapi.json would be different')
-        print('please regenerate openapi.json and the associated docs')
+        print("openapi.json would be different")
+        print("please regenerate openapi.json and the associated docs")
         print(str(ve))
         sys.exit(1)
 
